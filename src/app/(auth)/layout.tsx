7b7ea052f3d291c6a94d07bbe1730aa021064c20
Hanz <hanzchrisromechico@gmail.com>
2024-11-18 15:12:39 +0800

    Done UI for SignIn and SignUp

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+interface AuthlayoutProps {
+    children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthlayoutProps) => {
+    return ( 
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                    <Image src="/logo.svg" alt="Logo" width={152} height={56} />
+                    <div className="flex items-center gap-2">
+                        <Button variant="secondary">
+                            Sign Up
+                        </Button>
+                    </div>
+                </nav>
+                <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+                    {children}
+                </div>
+            </div>
+        </main>
+    );
+}
+ 
+export default AuthLayout;
