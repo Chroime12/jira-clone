e915db0d3bd0e6b1a1aec95da59dbcd808e94149
Hanz <hanzchrisromechico@gmail.com>
2024-11-21 10:39:37 +0800

    Working sending post request login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface AuthlayoutProps {
     children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthlayoutProps) => {
+    const pathname = usePathname();
+
     return ( 
         <main className="bg-neutral-100 min-h-screen">
             <div className="mx-auto max-w-screen-2xl p-4">
                 <nav className="flex justify-between items-center">
                     <Image src="/logo.svg" alt="Logo" width={152} height={56} />
                     <div className="flex items-center gap-2">
-                        <Button variant="secondary">
-                            Sign Up
+                        <Button asChild variant="secondary">
+                            <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+                                { pathname === "/sign-in" ? "Sign Up" : "Login" }
+                            </Link>
                         </Button>
                     </div>
                 </nav>
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -9,24 +9,27 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import Link from "next/link";
+import { loginSchema } from "../schemas";
+import { useLogin } from "../api/use-login";
 
-const formSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8, "Minimum 8 Characters"),
-})
-
-const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log({ values });
-}
 
 export const SignInCard = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const { mutate } = useLogin();
+
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
         defaultValues: {
             email: "",
             password: "",
         }
     })
+
+    const onSubmit = (values: z.infer<typeof loginSchema>) => {
+        mutate({
+            json: values
+        });
+    };
 
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
@@ -90,6 +93,17 @@ export const SignInCard = () => {
                     <FaGithub className="mr-2 size-5"/> Login with Github
                 </Button>
             </CardContent>
+            <div className="px-7">
+                <DottedSeperator />
+                <CardContent className="p-7 flex items-center justify-center">
+                    <p>
+                        Don&apos;t have an account?{" "}
+                        <Link href="/sign-up">
+                            <span className="text-blue-700"> Sign up </span>
+                        </Link>
+                    </p>
+                </CardContent>
+            </div>
         </Card>
     )
 }
