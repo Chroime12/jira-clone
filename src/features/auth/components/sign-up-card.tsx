f528e8280e2692f297eb28f6c5fbfc067c84a002
Hanz <hanzchrisromechico@gmail.com>
2024-11-21 13:07:23 +0800

    With login, register features

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -9,17 +9,15 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { registerSchema } from "../schemas";
+import { useRegister } from "../api/use-register";
 
-
-const formSchema = z.object({
-    name: z.string().trim().min(1, "Name is required"),
-    email: z.string().email(),
-    password: z.string().min(8, "Minimum 8 Characters required"),
-});
 
 export const SignUpCard = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const { mutate } = useRegister();
+
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
         defaultValues: {
             name: "",
             email: "",
@@ -27,8 +25,8 @@ export const SignUpCard = () => {
         },
     });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log({ values });
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
+        mutate({ json: values });
     };
 
     return (
